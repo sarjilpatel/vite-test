5a74d4a100f0fa507e2fd8b978f664867d5fd291
sarjil-zwt <sarjil@zealousweb.com>
2025-04-02 17:58:57 +0530

    added api call

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,38 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const About = () => {
-  return <div>About page</div>;
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const { data } = await axios.get("https://fakestoreapi.com/products");
+    setData(data);
+    setLoading(false);
+  };
+
+  return (
+    <div>
+      {loading ? (
+        <>Loading....</>
+      ) : (
+        <div>
+          {data.map((item: any) => {
+            return (
+              <div key={item?.id} className="dataitem">
+                <p>{item?.title}</p>
+                <p>{item?.price}</p>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default About;
